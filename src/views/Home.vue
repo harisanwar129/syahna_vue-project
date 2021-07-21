9cba88caca11bd4da55b4b4f1d40c1572fa17e62
harisanwar129 <harisanwar129@gmail.com>
2021-07-21 13:48:40 +0700

    covert_htm_home5

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <HeaderShayna />
     <HeroShayna />
+    <WomenShayna />
   </div>
 </template>
 
@@ -11,11 +12,13 @@
 // import HeaderShayna from "@/components/HeaderShayna.vue";
 import HeaderShayna from "../components/HeaderShayna.vue";
 import HeroShayna from "../components/HeroShayna.vue";
+import WomenShayna from "../components/WomenShayna.vue";
 export default {
   name: "Home",
   components: {
     HeaderShayna,
     HeroShayna,
+    WomenShayna,
   },
 };
 </script>
