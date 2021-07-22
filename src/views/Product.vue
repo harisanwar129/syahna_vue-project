09449aa2fa0fbe9fa91948005ae6f2afacb3c796
harisanwar129 <harisanwar129@gmail.com>
2021-07-22 10:53:25 +0700

    convert_rest_page

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -7,7 +7,7 @@
         <div class="row">
           <div class="col-lg-12">
             <div class="breadcrumb-text product-more">
-              <a href="./home.html"><i class="fa fa-home"></i> Home</a>
+              <router-link to="/"><i class="fa fa-home"></i> Home</router-link>
               <span>Detail</span>
             </div>
           </div>
@@ -98,8 +98,10 @@
                     <h4>$495.00</h4>
                   </div>
                   <div class="quantity">
-                    <a href="shopping-cart.html" class="primary-btn pd-cart"
-                      >Add To Cart</a
+                    <router-link to="/cart"
+                      ><a href="shopping-cart.html" class="primary-btn pd-cart"
+                        >Add To Cart</a
+                      ></router-link
                     >
                   </div>
                 </div>
@@ -110,6 +112,8 @@
       </div>
     </section>
     <!-- Product Shop Section End -->
+
+    <RelatedShayna />
     <FooterShayna />
   </div>
 </template>
@@ -119,14 +123,14 @@
 // import HelloWorld from "@/components/HelloWorld.vue";
 // import HeaderShayna from "@/components/HeaderShayna.vue";
 import HeaderShayna from "../components/HeaderShayna.vue";
-
+import RelatedShayna from "../components/RelatedShayna.vue";
 import FooterShayna from "../components/FooterShayna.vue";
 import carousel from "vue-owl-carousel";
 export default {
   name: "Product",
   components: {
     HeaderShayna,
-
+    RelatedShayna,
     FooterShayna,
     carousel,
   },
